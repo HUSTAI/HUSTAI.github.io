237bbf52e7bccbb5abb72a82da4cf8cb29028f1a
Liu Xiao <42756849+liuxiaocs7@users.noreply.github.com>
2023-06-12 16:40:06 +0800

    fix: path route (#3)

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -10,7 +10,7 @@ export default defineUserConfig({
       title: "Blog Demo",
       description: "A blog demo for vuepress-theme-hope",
     },
-    "/zh": {
+    "/zh/": {
       lang: "zh-CN",
       title: "博客演示",
       description: "vuepress-theme-hope 的博客演示",
