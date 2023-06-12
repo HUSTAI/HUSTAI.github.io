63be25348239ca646850c441a3c4ebabd91335bb
Liu Xiao <42756849+liuxiaocs7@users.noreply.github.com>
2023-06-12 16:16:20 +0800

    fix: revert lang setting (#2)
    * feat: change default lang
    
    * fix lang

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -5,12 +5,12 @@ export default defineUserConfig({
   base: "/",
 
   locales: {
-    "/us": {
+    "/": {
       lang: "en-US",
       title: "Blog Demo",
       description: "A blog demo for vuepress-theme-hope",
     },
-    "/": {
+    "/zh": {
       lang: "zh-CN",
       title: "博客演示",
       description: "vuepress-theme-hope 的博客演示",
