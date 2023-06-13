93d8f222bcbe342fc6243b445d7728f0c4f1a835
sheli00 <zhanglujun5@gmail.com>
2023-06-13 15:59:01 +0800

    comment force login

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -116,6 +116,7 @@ export default hopeTheme({
       // You should generate and use your own comment service
       provider: "Waline",
       serverURL: "https://waline-test-3k1u6a5k5-sheli00.vercel.app",
+      login: 'force'
     },
 
     // all features are enabled for demo, only preserve features you need here
