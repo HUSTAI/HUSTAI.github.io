d04c6c4f08266998a74c2afc3ae93135780d57ad
shbone <598924626@qq.com>
2023-06-13 09:04:17 +0800

    修改readme

--- node_modules/.pnpm/vidstack@0.6.9/node_modules/vidstack/dist/cdn/chunks/chunk-SZNEBIAP.js
+++
@@ -1,3 +0,0 @@
-var e={LoadFail:0,BadSignature:1,BadTimestamp:2,BadSettingValue:3,BadFormat:4,UnknownSetting:5},a=class extends Error{constructor(r){super(r.reason),this.code=r.code,this.line=r.line;}};
-
-export { e as a, a as b };
