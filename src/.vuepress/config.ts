6c6c6c63817e3899077d70a302804ada3058203f
sheli00 <zhanglujun5@gmail.com>
2023-06-16 11:07:38 +0800

    add searchPro

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
   base: "/",
@@ -22,7 +23,25 @@ export default defineUserConfig({
   },
 
   theme,
-
+  plugins: [
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      // 排除首页
+      isSearchable: (page) => page.path !== "/",
+      // 为分类和标签添加索引
+      customFields: [
+        {
+          getter: (page) => page.frontmatter.category,
+          formatter: "分类：$content",
+        },
+        {
+          getter: (page) => page.frontmatter.tag,
+          formatter: "标签：$content",
+        },
+      ]
+    })
+  ]
   // Enable it with pwa
   // shouldPrefetch: false,
 });
